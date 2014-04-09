169e0a50dfc5565e724f81469c43415fb9e3e2ba
Jean-Noël Chiganne <jn.chiganne@gmail.com>
2014-04-09 18:21:24 +0200

    VillageBehavior

---
+++ Assets/Scripts/VillageBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VillageBehavior : MonoBehaviour {
+
+	/** <summary>
+	  * The water required to save the village.
+	  * </summary>
+	  */
+	public int WaterRequired = 10;
+
+	private int rainQuantity;
+
+	// Use this for initialization
+	void Start () {
+		rainQuantity = 0;
+	}
+
+	void OnParticleCollision (GameObject other) {
+		++rainQuantity;
+		if (rainQuantity >= WaterRequired) {
+			Debug.Log ("Village Rescued");
+			this.collider.enabled = false;
+		}
+	}
+}
