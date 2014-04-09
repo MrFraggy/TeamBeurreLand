bffb694ad0064d82799caedda75a7969d0aaf371
Jean-Noël Chiganne <jn.chiganne@gmail.com>
2014-04-09 21:04:35 +0200

    Entonnoir : bouchage de trou

--- Assets/Scripts/EntonnoirBehaviour.cs
+++ Assets/Scripts/EntonnoirBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using MiddleVR_Unity3D;
 
 public class EntonnoirBehaviour : MonoBehaviour {
 
@@ -10,16 +11,30 @@ public class EntonnoirBehaviour : MonoBehaviour {
 	public float minRadius = 0.1f;
 	public float maxRadius = 0.5f;
 
+	private vrJoystick entonnoirHole;
 	private ParticleSystem waterSplash = null;
+	private float holeSize;
+	private float initialEmissionRate;
+
 	// Use this for initialization
 	void Start () {
 		waterSplash = gameObject.GetComponentInChildren<ParticleSystem> ();
 		waterSplash.enableEmission = false;
+		holeSize = 1.0f;
+		initialEmissionRate = waterSplash.emissionRate;
+		if (MiddleVR.VRDeviceMgr != null) {
+			entonnoirHole = MiddleVR.VRDeviceMgr.GetJoystick ("RazerHydra.Joystick0");
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		if (entonnoirHole == null)
+			return;
+		//Debug.Log (entonnoirHole.GetAxisNb ());
+		float axis = entonnoirHole.GetAxisValue(2);
+		holeSize = 1.0f - axis;
+		//Debug.Log ("Hole size : " + holeSize);
 	}
 
 	void OnParticleCollision(GameObject other) {
@@ -27,6 +42,7 @@ public class EntonnoirBehaviour : MonoBehaviour {
 			++waterQuantity;
 		else
 			waterSplash.enableEmission = true;
+		waterSplash.emissionRate = initialEmissionRate * holeSize;
 
 	}
 }
