dcadbf4c97b3babdfaf7c3af02adee59f2095981
Cecilia Lejeune <cecilia.lejeu@gmail.com>
2014-04-09 23:39:50 +0200

    Cloud motion

--- Assets/CloudMotion.cs
+++ Assets/CloudMotion.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class CloudMotion : MonoBehaviour {
 
 	public int m_maxCloud;
+	public Transform m_generationPoint;
+	public int m_generationWidth;
+	public Transform m_suppressionPoint;
+	public int m_suppressionWidth;
+	public int m_cloudGenerationHeight;
+	public float m_timeToWait;
 
 	private ArrayList m_clouds;
 
@@ -12,20 +18,35 @@ public class CloudMotion : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		m_clouds = new ArrayList();
+		StartCoroutine(instanciateClouds());
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		for(int i=0; i < m_clouds.Count; ++i){
+			GameObject cloud = (GameObject)m_clouds[i];
+			if((cloud.transform.position.x > m_suppressionPoint.position.x-m_suppressionWidth/2) && (cloud.transform.position.x < m_suppressionPoint.position.x+m_suppressionWidth/2)){
+				Debug.Log ("DESTROY");
+				m_clouds.RemoveAt(i);
+				Destroy(cloud);
+			}
+		}
+	}
 
-		float xPos = Random.Range(30.0f, -30.0f);
-		float zPos = Random.Range(35.0f, 45.0f);
-		Vector3 position = new Vector3(xPos, 20.0f, zPos);
-
-		if(m_clouds.Count < m_maxCloud){
-			GameObject cloud = (GameObject)Instantiate(m_cloudPrefab, position, Quaternion.identity);
-			cloud.transform.parent = transform;
-			cloud.transform.localScale = new Vector3(1f,1f,1f);
-			m_clouds.Add(cloud);
+	IEnumerator instanciateClouds(){
+		while(true){
+			float xPos = Random.Range(m_generationPoint.position.x + 10, m_generationPoint.position.x -10);
+			float zPos = Random.Range(m_generationPoint.position.z - m_cloudGenerationHeight/2, m_generationPoint.position.z + m_cloudGenerationHeight/2);
+			float yPos = Random.Range(m_generationPoint.position.y + m_generationWidth/2, m_generationPoint.position.y - m_generationWidth/2);
+			Vector3 position = new Vector3(xPos, 20 , zPos);
+			if(m_clouds.Count < m_maxCloud){
+				Debug.Log ("INSTANCIATE");
+				GameObject cloud = (GameObject)Instantiate(m_cloudPrefab, position, m_generationPoint.rotation);
+				cloud.transform.parent = transform;
+				cloud.transform.localScale = new Vector3(1f,1f,1f);
+				m_clouds.Add(cloud.gameObject);
+			}
+			yield return new WaitForSeconds(m_timeToWait);
 		}
 	}
 }
--- Assets/Scripts/CloudBehaviour.cs
+++ Assets/Scripts/CloudBehaviour.cs
@@ -16,18 +16,18 @@ public class CloudBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
 		//MOTION
 		if (transform.position.z < 0.0f && transform.position.z > -50.0f){
-			transform.position.Set (transform.position.x, transform.position.y + Time.deltaTime/10,transform.position.z + Time.deltaTime);
-		}else if(transform.position.z > 0.0f && transform.position.z < 50.0f){
-			transform.position.Set (transform.position.x, transform.position.y + Time.deltaTime/10,transform.position.z - Time.deltaTime);
+			transform.position += new Vector3(Time.deltaTime*Random.Range (1, 5), 0, 0);
+			//transform.TransformDirection(Quaternion.Euler(0.0f, (float)Random.Range (90, 180), 0.0f).eulerAngles);
+		}else if(transform.position.z >= 0.0f && transform.position.z < 50.0f){
+			transform.position +=  new Vector3(-Time.deltaTime*Random.Range (1, 5), 0, 0);
+			//transform.TransformDirection(Quaternion.Euler(0.0f, (float)Random.Range (90, 180), 0.0f).eulerAngles);
 		}
 
 		//RAIN
 		if(m_isRaining == true){
 			m_rainRate = m_rainRate - Time.deltaTime*10;
-			Debug.Log("RATE => " + m_rainRate);
 
 			if (m_rainRate <= 0.0f){
 
@@ -39,7 +39,6 @@ public class CloudBehaviour : MonoBehaviour {
 		}else if (m_isRaining ==false){
 
 			m_rainRate = m_rainRate + Time.deltaTime*10;
-			Debug.Log("RATE => " + m_rainRate);
 			if (m_rainRate >= 100.0f){
 				// Create rain
 				spawnRain(this.transform.position);
@@ -52,10 +51,10 @@ public class CloudBehaviour : MonoBehaviour {
 	//Instantiate the rain
 	void spawnRain(Vector3 position)
 	{
-		GameObject rain = (GameObject)Instantiate(m_rainPrefab, position, Quaternion.identity);
-		rain.transform.parent = transform;
-		rain.transform.rotation = Quaternion.Euler(90f,0f,0f);
-		rain.transform.localScale = new Vector3(1f,1f,1f);
+//		GameObject rain = (GameObject)Instantiate(m_rainPrefab, position, Quaternion.identity);
+//		rain.transform.parent = transform;
+//		rain.transform.rotation = Quaternion.Euler(90f,0f,0f);
+//		rain.transform.localScale = new Vector3(1f,1f,1f);
 		
 	}
 
