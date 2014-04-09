00a92a17fbd444c328fccf65e306fa4496749241
Jean-Noël Chiganne <jn.chiganne@gmail.com>
2014-04-09 22:51:14 +0200

    Entonnoir : remplissage de l'entonnoir en fonction de la quantité d'eau

--- Assets/Scripts/EntonnoirBehaviour.cs
+++ Assets/Scripts/EntonnoirBehaviour.cs
@@ -5,12 +5,14 @@ using MiddleVR_Unity3D;
 public class EntonnoirBehaviour : MonoBehaviour {
 
 	public float WaterCapacity = 10;
-	private float waterQuantity = 0;
+	public float MinWater = 0.2f;
 	public Vector3 minHeight = new Vector3(0.0f, 0.0f, 0.0f);
 	public Vector3 maxHeight = new Vector3(1.0f, 1.0f, 1.0f);
 	public float minRadius = 0.1f;
 	public float maxRadius = 0.5f;
+	public GameObject[] children = new GameObject[3];
 
+	private float waterQuantity = 0;
 	private vrJoystick entonnoirHole;
 	private ParticleSystem waterSplash = null;
 	private float holeSize;
@@ -38,11 +40,30 @@ public class EntonnoirBehaviour : MonoBehaviour {
 
 		if (waterQuantity > 0)
 			waterQuantity -= holeSize;
-		if (waterQuantity <= 0)
+		if (waterQuantity <= MinWater) {
 			waterSplash.enableEmission = false;
+			children [0].SetActive (false);
+			children [1].SetActive (false);
+			children [2].SetActive (false);
+		}
 		else {
 			waterSplash.startSpeed = initialStartSpeed * holeSize;
 			waterSplash.enableEmission = true;
+			if (waterQuantity < WaterCapacity / 3.0f) {
+				children[0].SetActive(true);
+				children[1].SetActive(false);
+				children[2].SetActive(false);
+			}
+			else if (waterQuantity < WaterCapacity / 3.0f * 2.0f) {
+				children[0].SetActive(false);
+				children[1].SetActive(true);
+				children[2].SetActive(false);
+			}
+			else {
+				children[0].SetActive(false);
+				children[1].SetActive(false);
+				children[2].SetActive(true);
+			}
 		}
 	}
 
