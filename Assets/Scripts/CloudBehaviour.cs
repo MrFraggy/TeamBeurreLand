07c0c184f1fb59afc425a0b3033bd53bc2d756f6
Cecilia Lejeune <cecilia.lejeu@gmail.com>
2014-04-10 09:15:39 +0200

    Entonnoir : gameplay fonctionnel

--- Assets/Scripts/CloudBehaviour.cs
+++ Assets/Scripts/CloudBehaviour.cs
@@ -6,17 +6,20 @@ public class CloudBehaviour : MonoBehaviour {
 	public float m_rainRate; // 0 : no rain, cloud white / 1 : about to rain, cloud black
 	public GameObject m_rainPrefab;
 	private bool m_isRaining;
-
+	private ParticleSystem particleSystem;
 	// Use this for initialization
 	void Start () {
 
 		m_rainRate = Random.Range(0, 100);
-		m_isRaining = false;
+		//m_isRaining = false;
+		spawnRain(this.transform.position);
+		particleSystem = gameObject.GetComponent<ParticleSystem>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		return;
 		//MOTION
 		if ((transform.position.x < 0.0f && transform.position.x > -50.0f) || (transform.position.x > 0.0f && transform.position.x < 50.0f)) {
 			transform.position += new Vector3 (Time.deltaTime * Random.Range (1, 5), 0, 0);
@@ -43,7 +46,8 @@ public class CloudBehaviour : MonoBehaviour {
 			}
 		}
 
-		gameObject.renderer.material.color = new Color(m_rainRate/10, m_rainRate/10, m_rainRate/10);
+		colorCloud(m_rainRate);
+
 	}
 
 	//Instantiate the rain
@@ -53,6 +57,16 @@ public class CloudBehaviour : MonoBehaviour {
 		rain.transform.parent = transform;
 		rain.transform.Rotate (0,90,0);
 		rain.transform.Translate (0, -5, 0);
+	}
+
+	void colorCloud(float m_rainRate){
+		float  colorIndex = m_rainRate;
+		if(colorIndex < 30){
+			colorIndex = 30;
+		}else if(colorIndex > 270){
+			colorIndex = 270;
+		}
+		gameObject.renderer.material.color = new Color(colorIndex/10, colorIndex/10, colorIndex/10);
 	}
 
 	// Suppress the rain
